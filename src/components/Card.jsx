e9762168c9790f4566ff554f7b632a4819346fb6
cosmopoliitans <elfkyn52@gmail.com>
2023-07-13 00:37:29 +0300

    css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -16,7 +16,7 @@ const Card = () => {
   return (
     <div className="w-1/3 h-full border fixed top-0 right-0 z-50 bg-white p-3">
       <div className="flex items-center h-20 justify-between">
-        <h1 className="text-2xl">SEPETİM:</h1>
+        <h1 className="text-2xl font-bold">MY BASKET :</h1>
         <AiOutlineClose
           onClick={() => dispatch({ type: "DRAWER", payload: false })}
           size={25}
